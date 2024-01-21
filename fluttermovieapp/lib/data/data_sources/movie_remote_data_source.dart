f09c0696d1722461bbb22d03afcbc49e4d73a56b
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2024-01-21 17:34:37 +0700

    up code lấy dữ liệu API

---
+++ fluttermovieapp/lib/data/data_sources/movie_remote_data_source.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'dart:html';
+import 'package:fluttermovieapp/data/core/api_clients.dart';
+import 'package:fluttermovieapp/data/core/api_constants.dart';
+import 'package:fluttermovieapp/data/models/movie_model.dart';
+import 'package:fluttermovieapp/data/models/movies_result_model.dart';
+import 'package:http/http.dart';
+
+abstract class MovieRemoteDataSource {
+  Future<List<MovieModel>> getTrending(); // gọi API, danh sách phim trending theo ngày
+  Future<List<MovieModel>> getPopular(); // gọi API, danh sách phim phổ biến 
+  Future<List<MovieModel>> getPlayingNow(); // gọi API, danh sách phim đang chiếu
+  Future<List<MovieModel>> getComingSoon(); // gọi API, danh sách phim sắp chiếu
+
+}
+
+class MovieRemoteDataSourceImpl extends MovieRemoteDataSource {
+  final ApiClient _client;
+  MovieRemoteDataSourceImpl(this._client);
+
+  @override
+  Future<List<MovieModel>> getTrending() async {  
+      final response = await _client.get('trending/movie/day');
+      final movies = MoviesResultModel.fromJson(response).movies;
+      print('\x1B[38;5;31mPhim trending\x1B[0m');
+      print(movies);
+      return movies ?? [];
+  }
+
+  @override
+  Future<List<MovieModel>> getPopular() async {
+      final response = await _client.get('movie/popular');
+      final movies = MoviesResultModel.fromJson(response).movies;
+      print('\x1B[38;5;31mPhim phổ biến\x1B[0m');
+      print(movies);
+      return movies ?? [];
+  }
+
+  @override
+  Future<List<MovieModel>> getComingSoon() async {
+      final response = await _client.get('movie/upcoming');
+      final movies = MoviesResultModel.fromJson(response).movies;
+      print('\x1B[38;5;31mPhim sắp chiếu\x1B[0m');
+      print(movies);
+      return movies ?? [];
+  }
+
+  @override
+  Future<List<MovieModel>> getPlayingNow() async {
+      final response = await _client.get('movie/now_playing');
+      final movies = MoviesResultModel.fromJson(response).movies;
+      print('\x1B[38;5;31mPhim đang chiếu\x1B[0m');
+      print(movies);
+      return movies ?? [];
+  }
+}
